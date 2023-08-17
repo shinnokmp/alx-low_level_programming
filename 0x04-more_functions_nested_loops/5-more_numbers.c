731616fd3b2718ab14a84d85cec386a05e6496ed
shinnokmp <shinnokswagg@gmail.com>
2023-08-17 13:14:49 +0000

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * more_numbers - nmbr fct
- * 
  * Return: no return
  */
 void more_numbers(void)
@@ -15,7 +14,7 @@ void more_numbers(void)
 		{	
 			if (j >= 10)
 				_putchar('1');
-				_putchar(j % 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
