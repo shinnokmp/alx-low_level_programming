165d4f2365bd250c3e450f5eb68bec2b3871abbf
shinnokmp <shinnokswagg@gmail.com>
2023-08-21 11:38:45 +0000

    Issues

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,12 @@ void puts2(char *str)
 	int string;
 
 	for (string = 0; str[string] != '\0'; string++)
+	{
 		if (string % 2 == 0)
 		{
 			_putchar(str[string]);
-			_putchar('\n');
+			/**_putchar('\n');*/
 		}
+	}
+	_putchar('\n');
 }
