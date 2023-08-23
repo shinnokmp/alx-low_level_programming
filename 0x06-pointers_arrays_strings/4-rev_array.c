d2db8f76cb4b035342fa50ae318c99a63cc2f12d
shinnokmp <shinnokswagg@gmail.com>
2023-08-23 14:14:08 +0000

    All files

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * @n:integer
+ * Return:void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, c;
+
+	for (i = 0; (i < (n - 1) / 2); i++)
+	{
+		c = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = c;
+	}
+}
