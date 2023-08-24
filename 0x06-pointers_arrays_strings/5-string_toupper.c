131bb1cd6c8e4ddfe54f0db4a6b7907ea1350cdb
shinnokmp <shinnokswagg@gmail.com>
2023-08-24 14:03:53 +0000

    Comment

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,21 +4,6 @@
  * @s:string
  * Return:char
  */
-/*char *string_toupper(char *str)
-
-{
-
-	int index = 0;
-
-	while (str[index])
-	{
-	if (str[index] >= 'a' && str[index] <= 'z')
-	str[index] -= 32;
-	index++;
-	}
-	return (str);
-
-}*/
 
 char *string_toupper(char *s)
 {
@@ -27,25 +12,9 @@ char *string_toupper(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
-		{	
+		{
 			s[i] = s[i] - 'a' + 'A';
 		}
 	}
 	return (s);
 }
-
-/*char *string_toupper(char *s)
-{
-	int i;
-	i = 0;
-
-	while (*(s + i))
-	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-		{
-			*(s + i) -= 'a' - 'A';
-		}
-	i++;
-	}
-	return (s);
-}*/
