682cb433bff90d501296a68f426303b13846be30
shinnokmp <shinnokswagg@gmail.com>
2023-08-24 10:59:32 +0000

    Duunno

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,6 +6,20 @@
  */
 char *string_toupper(char *s)
 {
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{	
+			s[i] = s[i] - 'a' + 'A';
+		}
+	}
+	return (s);
+}
+
+/*char *string_toupper(char *s)
+{
 	int i;
 	i = 0;
 
@@ -18,4 +32,4 @@ char *string_toupper(char *s)
 	i++;
 	}
 	return (s);
-}
+}*/
